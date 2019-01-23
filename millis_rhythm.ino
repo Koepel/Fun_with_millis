1ab6b55aeefcb7fe5d3f764f38268041eb2f0ef9
Koepel <Koepel@users.noreply.github.com>
2019-01-23 10:50:02 +0100

    Create millis_rhythm.ino

---
+++ millis_rhythm.ino
@@ -0,0 +1,59 @@
+// millis with a rhythm.
+//
+// Make a led blink with a specific rhythm, using millis.
+//
+
+const int ritme_table[] =
+{
+   160, 200,   // 160 ms on, 200 ms off
+   140, 200,
+   120, 200,
+   100, 200,
+    80, 1000,
+    80, 120,
+    80, 120,
+    80, 1000,
+  1000, 500,
+};
+
+unsigned long previousMillis;
+int index = 0;
+
+const int ledPin = 13;
+
+
+void setup()
+{
+  pinMode( ledPin, OUTPUT);
+  
+  // The table starts with the led on.
+  // After 160 ms it will be turned off.
+  digitalWrite( ledPin, HIGH);
+  previousMillis = millis();
+}
+
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  
+  if( currentMillis - previousMillis >= ritme_table[index])
+  {
+    previousMillis = currentMillis;
+    
+    if( index % 2 == 0)  // check if millis is an even number
+    {
+      digitalWrite( ledPin, LOW);
+    }
+    else
+    {
+      digitalWrite( ledPin, HIGH);
+    }
+    
+    index++;
+    if( index >= sizeof(ritme_table) / sizeof(int))
+    {
+      index = 0;
+    }
+  }
+}
