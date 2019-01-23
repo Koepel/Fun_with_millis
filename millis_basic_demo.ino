935eb18211fd062f7b8aa7d0a5707e20566f680d
Koepel <Koepel@users.noreply.github.com>
2019-01-23 10:15:15 +0100

    Create millis_basic_demo.ino

---
+++ millis_basic_demo.ino
@@ -0,0 +1,50 @@
+// millis_basic_demo.ino
+//
+// Running three different things with millis().
+// The serial monitor is used to show the three different things.
+//
+
+unsigned long previousMillisDot;
+const unsigned long intervalDot = 200;
+
+unsigned long previousMillisHello;
+const unsigned long intervalHello = 1600;
+
+unsigned long previousMillisRandom;
+unsigned long intervalRandom = 1000;
+
+void setup()
+{
+  Serial.begin( 9600);
+  Serial.println();
+  Serial.println( "The sketch has started");
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  
+  if( currentMillis - previousMillisDot >= intervalDot)
+  {
+    previousMillisDot = currentMillis;
+    
+    Serial.print( ".");
+  }
+  
+  if( currentMillis - previousMillisHello >= intervalHello)
+  {
+    previousMillisHello = currentMillis;
+    
+    Serial.println();
+    Serial.print( "Hello");
+  }
+  
+  if( currentMillis - previousMillisRandom >= intervalRandom)
+  {
+    previousMillisRandom = currentMillis;
+    
+    intervalRandom = (unsigned long) random( 500, 5000);
+    
+    Serial.print( "Random");
+  }
+}
