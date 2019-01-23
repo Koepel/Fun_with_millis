581120d937fbcf54c0fc5050bfeb8d0164ce1150
Koepel <Koepel@users.noreply.github.com>
2019-01-23 10:24:25 +0100

    Create millis_single_delay.ino

---
+++ millis_single_delay.ino
@@ -0,0 +1,46 @@
+// One shot software timer with millis().
+// Turn on a led for 4 seconds when a switch is used.
+// 
+// The 4 seconds starts when the switch is released.
+// When the timing should start at the moment the
+// switch is pressed, then the State Change Detection
+// should be added.
+//
+// The switch is connected to pin 2 and GND.
+//
+// The led is the led on the Arduino board,
+// any other digital pin can be used.
+//
+// Created and tested at Tinkercad.com
+
+const int switchPin = 2;
+
+unsigned long previousMillis;
+const unsigned long interval = 4000;
+bool enabled = false;
+
+void setup()
+{
+  pinMode( switchPin, INPUT_PULLUP);
+  pinMode( LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  if( digitalRead( switchPin) == LOW)  // low is active
+  {
+    digitalWrite( LED_BUILTIN, HIGH);  // turn on led
+    previousMillis = millis();
+    enabled = true;        // turn on software timer
+  }
+  
+  
+  if( enabled)      // software timer is active ?
+  {
+    if( millis() - previousMillis >= interval)
+    {
+      digitalWrite( LED_BUILTIN, LOW);  // turn off led
+      enabled = false;     // stop software timer
+    }
+  }      
+}
