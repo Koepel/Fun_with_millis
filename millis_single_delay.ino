105028a9a87080fe10e1b1eacb00d2e240a6ea26
Koepel <Koepel@users.noreply.github.com>
2019-02-05 23:09:38 +0100

    Update millis_single_delay.ino

--- millis_single_delay.ino
+++ millis_single_delay.ino
@@ -16,12 +16,13 @@
 //
 // The switch is connected to pin 2 and GND.
 //
-// The led is the led on the Arduino board,
+// The LED_BUILTIN led is the led on the Arduino board,
 // any other digital pin can be used.
 //
 // Created and tested at Tinkercad.com
 
 const int switchPin = 2;
+const int ledPin = LED_BUILTIN;
 
 unsigned long previousMillis;
 const unsigned long interval = 4000;
@@ -30,25 +31,27 @@ bool enabled = false;
 void setup()
 {
   pinMode( switchPin, INPUT_PULLUP);
-  pinMode( LED_BUILTIN, OUTPUT);
+  pinMode( ledPin, OUTPUT);
 }
 
 void loop()
 {
+  unsigned long currentMillis = millis();
+  
   if( digitalRead( switchPin) == LOW)  // low is active
   {
-    digitalWrite( LED_BUILTIN, HIGH);  // turn on led
-    previousMillis = millis();
+    digitalWrite( ledPin, HIGH);  // turn on led
+    previousMillis = currentMillis;
     enabled = true;        // turn on software timer
   }
   
   
   if( enabled)      // software timer is active ?
   {
-    if( millis() - previousMillis >= interval)
+    if( currentMillis - previousMillis >= interval)
     {
-      digitalWrite( LED_BUILTIN, LOW);  // turn off led
+      digitalWrite( ledPin, LOW);  // turn off led
       enabled = false;     // stop software timer
     }
-  }      
+  }
 }
