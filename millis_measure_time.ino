0ca675003ad34046188ea3f2cae5fa95dfceb3d0
Koepel <Koepel@users.noreply.github.com>
2019-07-20 16:15:13 +0200

    Version 2

--- millis_measure_time.ino
+++ millis_measure_time.ino
@@ -3,14 +3,19 @@
 // ---------------------------------------------
 // License: The Unlicense, Public Domain.
 // Author: Koepel
-// 2019 january 24
+// Version 1, 2019 january 24
+// Version 2, 2019 july 20
+//   The sketch was not accurate for short times.
+//   For example when measuring how long a digitalWrite() or
+//   digitalRead() takes. Now using micros() instead of millis()
+//   for the part that executes a function 1000 times.
 // ---------------------------------------------
 // 
 // Use millis() and micros() to measure how long a piece of code takes.
 //
 // The Serial.flush() is used to wait until the data is transmitted,
 // so there will be no more interrupts from the serial bus.
-// To be sure the Serial.flush() is also followed by a delay.
+// To be sure, the Serial.flush() is also followed by a delay.
 
 
 // The macro X1000 executes something 1000 times.
@@ -21,12 +26,12 @@
 void setup()
 {
   Serial.begin( 9600);
-  Serial.println( "Using millis() to measure how long a piece of code takes.");
+  Serial.println( "Using millis() and micros() to measure how long a piece of code takes.");
   Serial.println( "Column 1: Calculated pi.");
   Serial.println( "Column 2: The time the calculation of pi took in milliseconds.");
   Serial.println( "Column 3: A single analogRead with micros() with interrupts disabled.");
   Serial.println( "Column 4: A single analogRead with micros() and interrupts on.");
-  Serial.println( "Column 5: Thousand analogRead with millis.");
+  Serial.println( "Column 5: Thousand analogRead with micros().");
   Serial.flush();
   delay( 100);
 }
@@ -37,9 +42,13 @@ void loop()
   unsigned long t1, t2, result;
 
   // -------------------------------------------------
-
   // Basic measurement.
   // Measure how long the function "calcPi" takes in milliseconds.
+  // Using millis() with interrupts enabled.
+  // The millis() will not work without interrupts enabled.
+  // -------------------------------------------------
+
+  Serial.print( "pi=");
 
   t1 = millis();           // get millis
   float f = calcPi();      // call the function under test
@@ -47,19 +56,19 @@ void loop()
   result = t2 - t1;        // calcuate the difference
 
   Serial.print( f, 7);     // print the calculated value of pi as well
-  Serial.print( ",  ");
+  Serial.print( ",   ");
   Serial.print( result);
-  Serial.print( ",  ");
+  Serial.print( " ms,   ");
   Serial.flush();
   delay( 100);
 
   // -------------------------------------------------
-  
   // Using micros() to calculate how long the function analogRead() takes.
   // The interrupts are turned off for better accuracy.
   // This will work on a Arduino Uno, but might not work on all boards.
   // For an Arduino Uno, the micros() is not accurate to the microsecond.
-  
+  // -------------------------------------------------
+
   noInterrupts();
   t1 = micros();
   analogRead(A0);
@@ -68,49 +77,48 @@ void loop()
   result = t2 - t1;
 
   Serial.print( result);
-  Serial.print( ",  ");
+  Serial.print( " µs,   ");
   Serial.flush();
   delay( 100);
 
   // -------------------------------------------------
-
   // Using micros() with interrupts enabled
-  
+  // -------------------------------------------------
+
   t1 = micros();
   analogRead(A0);
   t2 = micros();
   result = t2 - t1;
 
   Serial.print( result);
-  Serial.print( ",  ");
+  Serial.print( " µs,   ");
   Serial.flush();
   delay( 100);
 
   // -------------------------------------------------
-
-  // Using millis() with interrupts enabled.
-  // The millis() will not work without interrupts enabled.
   // The function that is tested is executed 1000 times.
-  // The resulting time could be divided by 1000, or the result could be seen as microseconds.
+  // The resulting time could be divided by 1000 for microseconds, 
+  // or the result could be seen as nanoseconds.
   // Because the function under test is executed 1000 times, the glitches by interrupts
-  // are averaged, and overhead has less influence, this gives a more steady result.
+  // are averaged, this gives a more steady result.
+  // -------------------------------------------------
 
-  t1 = millis();
-  X1000(analogRead(A0));       // The X1000 macro expands it thousand times
-  t2 = millis();
+  t1 = micros();
+  X1000( analogRead( A0));       // The X1000 macro expands it thousand times
+  t2 = micros();
   result = t2 - t1;
 
-  Serial.print( result);
-  Serial.print( ",  ");
-  Serial.flush();
-  delay( 100);
-
-  // -------------------------------------------------
-  
-  Serial.println();
+  Serial.print( result / 1000);
+  Serial.print( ".");
+  unsigned long remainder = result % 1000;
+  if( remainder < 100)  Serial.print( "0");
+  if( remainder < 10)   Serial.print( "0");
+  Serial.print( remainder);
+  Serial.println( " µs");
   Serial.flush();
   delay( 100);
 }
+
 
 // Madhava-Leibniz formula.
 float calcPi()
